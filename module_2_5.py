7f46a1e741c493309c93a2aa1a8ac510ad5ca7fb
vy541 <uumowd1@icloud.com>
2024-10-15 13:22:25 +0300

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,11 @@
+def get_matrix(n, m, value):  # Объявите функцию get_matrix и напишите в ней параметры n, m и value.
+    matrix = []  # Создайте пустой список matrix внутри функции get_matrix.
+    for i in range(n):  # Напишите первый(внешний) цикл for для кол-ва строк матрицы, n повторов.
+        matrix.append([])  # В первом цикле добавляйте пустой список в список matrix.
+        for j in range(m):  # Напишите второй(внутренний) цикл for для кол-ва столбцов матрицы, m повторов.
+            matrix[i].append(value)  # Во втором цикле пополняйте ранее добавленный пустой список значениями value.
+    return matrix
+print(get_matrix(2,2,10))
+print(get_matrix(3,5,42))
+print(get_matrix(4,2,13))
+
